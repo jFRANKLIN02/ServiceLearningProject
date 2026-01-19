07ec9b187ce5ce3e9e784a4595c2df5661489098
ethev31 <ethanevans_31@yahoo.com>
2026-01-19 14:58:24 -0400

    project created

---
+++ ServiceLearningReporting/src/main/java/org/example/servicelearningreporting/ServiceLearningReportingApplication.java
@@ -0,0 +1,13 @@
+package org.example.servicelearningreporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLearningReportingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceLearningReportingApplication.class, args);
+    }
+
+}
