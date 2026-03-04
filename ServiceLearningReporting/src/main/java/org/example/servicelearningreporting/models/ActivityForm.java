55c71de21498b480a969d67413c61a1cf5c8aca2
JarenFranklin <jfranklin-1@mynbcc.ca>
2026-03-03 21:30:23 -0400

    Small change, removed all errors on forms

--- ServiceLearningReporting/src/main/java/org/example/servicelearningreporting/models/ActivityForm.java
+++ ServiceLearningReporting/src/main/java/org/example/servicelearningreporting/models/ActivityForm.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -21,6 +22,7 @@ public class ActivityForm {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private long id;
     private LocalDateTime timestamp;
+    @DateTimeFormat(pattern="yyy-MM-dd")
     private LocalDate date;
     private LocalTime startTime;
     private LocalTime endTime;
