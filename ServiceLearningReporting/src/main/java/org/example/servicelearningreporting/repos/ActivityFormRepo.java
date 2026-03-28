1ccf049ec2ad9b88f5ecf92266a028abb81bb842
ethev31 <ethanevans_31@yahoo.com>
2026-03-27 21:15:53 -0300

    added nbcc logo to header.html and pagination to submittedForms.html and inProgressForms.html

--- ServiceLearningReporting/src/main/java/org/example/servicelearningreporting/repos/ActivityFormRepo.java
+++ ServiceLearningReporting/src/main/java/org/example/servicelearningreporting/repos/ActivityFormRepo.java
@@ -2,6 +2,8 @@ package org.example.servicelearningreporting.repos;
 
 import org.example.servicelearningreporting.models.ActivityForm;
 import org.jspecify.annotations.Nullable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +11,5 @@ import java.util.List;
 public interface ActivityFormRepo extends JpaRepository<ActivityForm, Long>{
     @Nullable Object findActivityFormBySubmitted(boolean b);
     List<ActivityForm> findBySubmitted(Boolean submitted);
-    List<ActivityForm> findBySubmittedAndUserID(boolean submitted, Integer userID);
+    Page<ActivityForm> findBySubmittedAndUserID(boolean submitted, Integer userID, Pageable pageable);
 }
